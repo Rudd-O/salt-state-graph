651f047c98ab1d50e8575fa79be9add0f5a2032c
Manuel Amador (Rudd-O) <rudd-o@rudd-o.com>
2022-04-20 01:06:46 +0000

    Tag 0.0.4.

--- setup.py
+++ setup.py
@@ -4,29 +4,23 @@ import uuid
 import setuptools
 
 
-VERSION = '0.0.3'
+VERSION = "0.0.4"
 
-test_requirements = ['pytest==2.7.2', 'pytest-cov==2.0.0', 'flake8']
+test_requirements = ["pytest==2.7.2", "pytest-cov==2.0.0", "flake8"]
 
 setup = dict(
-    author='Cera Davies',
-    author_email='ceralena.davies@learnosity.com',
-    url='https://github.com/ceralena/salt-state-graph',
+    author="Cera Davies",
+    author_email="ceralena.davies@learnosity.com",
+    url="https://github.com/ceralena/salt-state-graph",
     version=VERSION,
-    name='salt_state_graph',
-    description='a tool to represent salt states and their dependencies as an acyclic graph',
+    name="salt_state_graph",
+    description="a tool to represent salt states and their dependencies as an acyclic graph",
     packages=[
-        'salt_state_graph',
+        "salt_state_graph",
     ],
-
-    scripts=[
-        'bin/salt_state_graph'
-    ],
-
+    scripts=["bin/salt_state_graph"],
     tests_require=test_requirements,
-
     install_requires=["pydot"],
-
     include_package_data=True,
 )
 
